d84ec3290c6854dab8de7fcad5048822583e849d
vagrant <Vivian Zhang>
2022-03-09 23:44:26 +0000

    Implement the http basic auth to all admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ["Jungle"], password: ENV["book"], only: [:admin]
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with username: "Jungle", password: "book", only: [:admin]
 
   def index
     @products = Product.order(id: :desc).all
