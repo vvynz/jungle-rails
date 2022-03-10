f31cda2d241b777dc8c4ff6842d41888542f1a13
vagrant <Vivian Zhang>
2022-03-10 19:52:11 +0000

    Add index, new, and create actions to admin/categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,13 @@
 class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+  end
 end
