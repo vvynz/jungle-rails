85416c89ff85b5b49c987882a99085af4379ec00
vagrant <Vivian Zhang>
2022-03-11 01:12:27 +0000

    Bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    parms.require(:user).permit(
+    params.require(:user).permit(
       :first_name,
       :last_name,
       :email,
