a435a05a5a01a5626caf24399ab653c67de4245e
vagrant <Vivian Zhang>
2022-03-11 00:30:03 +0000

    Creaete a private user_params method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
-  
+
   def new
   end
 
   def create
+  end
+
+  private
+
+  def user_params
+    parms.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
