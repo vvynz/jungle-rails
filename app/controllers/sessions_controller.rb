a720e06f32cfd9c94f9a9f02831750670354fb5a
vagrant <Vivian Zhang>
2022-03-11 00:58:13 +0000

    Update and implement the create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    # if the user exists AND password is correct
+    if user && user.authenticate(params[:password])
+      # save the user id inside browser cookie
+      session[:user_id] = user_id
+      redirect_to '/'
+    else
+      # if login doesn't work, redirect back to login
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
