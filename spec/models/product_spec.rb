4199841bfd1def2ea453da31d7193b82c82aa782
vagrant <vvynz12@gmail.com>
2022-03-14 20:43:31 +0000

    All spec tests passing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,27 +16,39 @@ RSpec.describe Product, type: :model do
 
     it 'is not valid without a name' do
       @category = Category.create(name: "food")
-      # @product = Product.new(name: nil, price_cents: 50, quantity: 100, category: @category)
       @product = Product.new(name: nil)
+      @product.save
     
 
       expect(@product).to_not be_valid
-      expect(@product.errors[:name].size).to eq(1)
+      # expect(@product.errors[:name].size).to eq(1)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is not valid without a price' do
-      @product = Product.new(name: 'fried chicken', price_cents: nil, quantity: 100)
+      @category = Category.create(name: "food")
+      @product = Product.new(price_cents: nil)
+      @product.save
 
-      expect(@product.price_cents).to_not be_present
-      # expect(@product.errors.full_messages).to include("Price can't be blank")
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'is not valid without a quantity' do
-      
+      @category = Category.create(name: "food")
+      @product = Product.new(quantity: nil)
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
-      
+      @product = Product.new(category: nil)
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
