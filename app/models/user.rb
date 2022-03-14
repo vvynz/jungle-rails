d409e8990de09d77e29e77343e3d3ab540faf97b
vagrant <vvynz12@gmail.com>
2022-03-14 23:56:47 +0000

    Implement the authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  def authenticate_with_credentials(email, password)
+  def self.authenticate_with_credentials(email, password)
+    # edge case: change the email to lowercase & remove any whitespace
+    user_email = email.downcase.strip
+
+    @user = User.find_by_email(user_email)
+    if @user && @user.authenticate(password)
+      # if success, logs the user in
+      @user
+    else
+      nil
+    end
   end
 end
