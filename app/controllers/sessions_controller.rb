916a878b09718b1d901635ae9790698716025202
vagrant <vvynz12@gmail.com>
2022-03-15 00:01:43 +0000

    Update with new authenticate_with_credentials method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,25 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # def create
+  #   user = User.find_by_email(params[:email])
+
+  #   # if the user exists AND password is correct
+  #   if user && user.authenticate(params[:password])
+  #     # save the user id inside browser cookie
+  #     session[:user_id] = user.id
+  #     redirect_to '/'
+  #   else
+  #     # if login doesn't work, redirect back to login
+  #     redirect_to '/login'
+  #   end
+  # end
+
   def create
     user = User.find_by_email(params[:email])
 
-    # if the user exists AND password is correct
-    if user && user.authenticate(params[:password])
-      # save the user id inside browser cookie
+    if user && user.authenticate_with_credentials(params[:password])
+      # success, saves the user id inside browser cookie
       session[:user_id] = user.id
       redirect_to '/'
     else
