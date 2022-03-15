8af68a133882b197503ee25deda07fb3ce38fc43
vagrant <vvynz12@gmail.com>
2022-03-15 18:17:48 +0000

    First capybara test: user should see products on the page

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+# RSpec.feature "HomePages", type: :feature do
+#   pending "add some scenarios (or delete) #{__FILE__}"
+# end
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product'
+  end
+end
