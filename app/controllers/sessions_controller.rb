d3d18f08f23f6560c2de9b8dbff6aad930e73653
vagrant <vvynz12@gmail.com>
2022-03-15 00:05:26 +0000

    minor update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,25 +3,12 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # def create
-  #   user = User.find_by_email(params[:email])
-
-  #   # if the user exists AND password is correct
-  #   if user && user.authenticate(params[:password])
-  #     # save the user id inside browser cookie
-  #     session[:user_id] = user.id
-  #     redirect_to '/'
-  #   else
-  #     # if login doesn't work, redirect back to login
-  #     redirect_to '/login'
-  #   end
-  # end
-
   def create
     user = User.find_by_email(params[:email])
 
-    if user && user.authenticate_with_credentials(params[:password])
-      # success, saves the user id inside browser cookie
+    # if the user exists AND password is correct
+    if user && user.authenticate(params[:password])
+      # save the user id inside browser cookie
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -29,6 +16,19 @@ class SessionsController < ApplicationController
       redirect_to '/login'
     end
   end
+
+  # def create
+  #   user = User.find_by_email(params[:email])
+
+  #   if user && user.authenticate_with_credentials(params[:password])
+  #     # success, saves the user id inside browser cookie
+  #     session[:user_id] = user.id
+  #     redirect_to '/'
+  #   else
+  #     # if login doesn't work, redirect back to login
+  #     redirect_to '/login'
+  #   end
+  # end
 
   def destroy
     session[:user_id] = nil
